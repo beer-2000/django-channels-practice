16d3737f8d10ba25a70b59c85e065b88467b6736
Lee Woojin <dhkep03@gmail.com>
2022-05-20 00:59:02 +0900

    video 4 완료 - 메세지에 sender, receiver 정보 담기

--- chat/settings.py
+++ chat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-((%kjjz7+-$r27j*^pm*__1$r$enrho6h6rfw2lnskrv72pk57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
